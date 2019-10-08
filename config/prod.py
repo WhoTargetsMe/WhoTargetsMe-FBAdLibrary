ee40697520703f6140c60845ba4a41ee964ac92c
EV <vzhenia@users.noreply.github.com>
2019-10-08 21:48:25 +0300

    Prod db settings

--- config/prod.py
+++ config/prod.py
@@ -1,10 +1,11 @@
 import os
 
-DEBUG=False
+DEBUG=True
 SECRET_KEY='prodsecret'
 # postgresql://<username>:<userpassword>@<url>/<dbname>
 # postgresql://postgres:MeTargetsWho?@wtm-ad-library-stage.cfhyzec3tjdz.eu-central-1.rds.amazonaws.com/fbadlibraryclone
-SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+# SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:MeTargetsWho?@wtm-ad-library-stage.cfhyzec3tjdz.eu-central-1.rds.amazonaws.com/fbadlibraryclone'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 APP_ID='1969644276620014'
 APP_SECRET='8f12eb11035256f47ed1be33974967a6'
@@ -16,6 +17,6 @@ LONG_TOKEN_EXPIRES_IN=5184000
 # How many pages we want to fetch between each storing operation
 PAGES_BETWEEN_STORING = 10
 # Each page may contain 25-5000 ads, but tests show that they don't permit to fetch 5000
-ADS_PER_PAGE = 500
+ADS_PER_PAGE = 200
 # Interval of cron job in seconds
 INTERVAL = 3600*24
