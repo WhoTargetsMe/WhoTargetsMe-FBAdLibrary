a1c8667db385457fe25a1e78404a062f8be975ce
Kevin Carmody <kevin@skinofstars.com>
2019-11-29 22:02:33 +0000

    Ad insert speedup
    A significant refactor of ad insert code to use SQLAlchemy core.
    
    - >4x speedup seen on single advertiser with 2688 ads
    - Noticable memory reduction... ~100MB less per 2000 ads:

--- application.py
+++ application.py
@@ -1,22 +1,23 @@
 from app import create_app, db
-from app.service.models import Advertisers, Tokens
-
-import sys
-from apscheduler.schedulers.background import BackgroundScheduler
 from app.fbconnector.ad_downloader import download_ads
 from app.service.models import Advertisers, Adverts, Tokens
-from app.utils.loader import parse_and_load_adverts
+from app.service.models import Advertisers, Tokens
+from app.utils.ad_inserter import parse_and_insert
 from app.utils.functions import finished_main_scripts
-from flask import render_template, request, session, g, jsonify
-from flask import current_app as ap
-from sqlalchemy import exc, func
-import requests
+from app.utils.loader import parse_and_load_adverts
+from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
+from flask import current_app as ap
+from flask import render_template, request, session, g, jsonify
+from sqlalchemy import exc, func
+import os
+import requests
+import sys
 
 def call_loader(country='GB'):
     with application.app_context():
         print('starting loading job', datetime.now())
-        single_call_lst = []
+        
         # Get config to make request to FB library
         API_VERSION = ap.config['API_VERSION']
         PAGES_BETWEEN_STORING = ap.config['PAGES_BETWEEN_STORING']
@@ -24,16 +25,25 @@ def call_loader(country='GB'):
         latest_record = db.session.query(Tokens).order_by(Tokens.id.desc()).first()
         LONG_TOKEN = latest_record.long_token
 
+        # get our predefined advertisers
         advertisers = Advertisers.query.all()
-        IDS = [int(a.page_id) for a in advertisers if a.country == country]
+        advertiser_ids = [int(a.page_id) for a in advertisers if a.country == country]
+        
+        # get the most frequent advertisers, based on previously saved adverts
+        single_call_lst = []
         adverts = db.session.query(Adverts.page_id, func.count(Adverts.page_id)).group_by(Adverts.page_id).all()
         single_call_lst = [int(a[0]) for a in adverts if a[1] < (ADS_PER_PAGE - 100)]
+        ordered_ids = sorted([a for a in adverts], key=lambda k: k[1], reverse=True)
+        frequent_advertiser_ids = [int(a[0]) for a in ordered_ids][:2] #50
+
+        # select which advertiser ids set to use, based on... time of day?
         if finished_main_scripts():
-            ordered_ids = sorted([a for a in adverts], key=lambda k: k[1], reverse=True)
-            IDS = [int(a[0]) for a in ordered_ids][:2] #50
+            IDS = frequent_advertiser_ids
+        else:
+            IDS = advertiser_ids
+
         print('FETCHING IDS ...', IDS)
         print('single_call_lst', single_call_lst)
-
 
         for ID in IDS:
             # Iteratively download and store ads
@@ -44,8 +54,11 @@ def call_loader(country='GB'):
                 print('...CRON...Starting download from FB library.................', ID, 'time=', datetime.now())
                 body, next_page = download_ads(API_VERSION, LONG_TOKEN,\
                     PAGES_BETWEEN_STORING, ADS_PER_PAGE, [ID], country, next_page, single_call)
+                
                 print('...CRON.....Uploading data to DB....................', ID, 'page=', page, 'time=', datetime.now())
-                parse_and_load_adverts(body, country)
+                # parse_and_load_adverts(body, country)
+                parse_and_insert(body, country)
+
                 page += 1
             print('...CRON...Finished upload to db.............................', ID, 'time=', datetime.now())
         return '<h1> Advertisers: {0} </h1>'.format(IDS)
@@ -54,7 +67,7 @@ def call_loader(country='GB'):
 #     raise ValueError("Pls provide ENV")
 
 PARAMS = {
-    'ENV': 'prod' # 'dev', 'prod'
+    'ENV': os.getenv("ENV", 'prod') # 'dev', 'prod'
 }
 try:
     for i, arg in enumerate(sys.argv):
