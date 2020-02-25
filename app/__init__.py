188dcefe3c456c5800806e88999c1902fa8f7dbb
Kevin Carmody <kevin@skinofstars.com>
2020-02-25 12:43:52 +0000

    Fix and tidy up scheduler

--- app/__init__.py
+++ app/__init__.py
@@ -3,35 +3,31 @@ from datetime import datetime
 from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
+from flask_apscheduler import APScheduler
 import os
 
+scheduler = APScheduler()
 db = SQLAlchemy()
 
 def create_app(config_type):
-    application = Flask(__name__)
-    
-    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
-    application.config.from_pyfile(configuration)
+    app = Flask(__name__)
 
-    db.init_app(application)
-    migrate = Migrate(application, db)
+    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
+    app.config.from_pyfile(configuration)
+
+    db.init_app(app)
+    Migrate(app, db)
 
     from app.service import main
-    application.register_blueprint(main)
+    app.register_blueprint(main)
 
-    return application
+    scheduler.init_app(app)
+    scheduler.start()
+    from app import tasks
 
-from app.service.routes import call_loader
-from apscheduler.schedulers.background import BackgroundScheduler
+    return app
 
-application = create_app(os.getenv("FLASK_ENV", 'production'))
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(call_loader, 'cron', month='*', day='*', hour='1,7,10,15,19', minute='1')
-scheduler.start()
+app = create_app(os.getenv("FLASK_ENV", 'production'))
 
 if __name__ == '__main__':
-    with application.app_context():
-        db.create_all()
-
-        application.run()
+    app.run()
