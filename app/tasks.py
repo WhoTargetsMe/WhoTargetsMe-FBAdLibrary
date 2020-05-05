5224eca51fc29b01174c3a2920fa99ae278d6489
Kevin Carmody <kevin@skinofstars.com>
2020-05-05 19:38:25 +0100

    GB, US crawl tasks

--- app/tasks.py
+++ app/tasks.py
@@ -2,8 +2,16 @@ from . import scheduler
 
 
 @scheduler.task("cron", month="*", day="*", hour="1,9,18", minute="8")
-def run_task():
+def run_task_US():
     with scheduler.app.app_context():
         from app.service.routes import call_loader
 
-        call_loader()
+        call_loader(country="US")
+
+
+@scheduler.task("cron", month="*", day="*", hour="22", minute="8")
+def run_task_GB():
+    with scheduler.app.app_context():
+        from app.service.routes import call_loader
+
+        call_loader(country="GB")
