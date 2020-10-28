e286217ce805b8715293f397e2181025ed3702e8
Kevin Carmody <kevin@skinofstars.com>
2020-10-28 00:22:39 +0000

    Hack in task to update on all currently running ads

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,6 @@
 from . import scheduler
 from app.utils.refresh_mat_views import refresh_all_mat_views
+import datetime
 
 
 @scheduler.task("cron", month="*", day="*", hour="*", minute="8")
@@ -7,7 +8,18 @@ def run_task_US():
     with scheduler.app.app_context():
         from app.service.routes import call_loader
 
-        call_loader(country="US")
+        # TODO remove `search`
+        # TODO remove `all_ads`
+
+        # aggressive update on all current ads
+        now = datetime.datetime.now()
+        today_date_string = now.strftime("%Y-%m-%d")
+        search = dict(
+            ad_delivery_date_min=today_date_string,
+            ad_delivery_date_max=today_date_string,
+        )
+
+        call_loader(country="US", search=search, all_ads=True)
 
 
 @scheduler.task("cron", month="*", day="*", hour="22", minute="8")
